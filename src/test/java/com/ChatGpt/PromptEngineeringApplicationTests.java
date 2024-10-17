a6fb0e1d7a436d6fe3e32a59cc622e734ecb397c
Junyeong-An <2117ab@gmail.com>
2024-10-18 00:08:01 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/ChatGpt/PromptEngineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChatGpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptEngineeringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
